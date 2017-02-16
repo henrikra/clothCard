9aba883c58cc905600f0880f501ea7924c9ebe27
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-02-16 20:08:11 +0200

    Add images

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  ScrollView,
+  Image,
+  Dimensions,
+  Platform,
 } from 'react-native';
 
 import Card from './Card';
@@ -11,7 +15,34 @@ class clothCard extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.images} />
+        <View style={styles.images}>
+          <ScrollView
+            horizontal 
+            pagingEnabled 
+            showsHorizontalScrollIndicator={false}
+          >
+            <Image 
+              style={styles.image} 
+              resizeMode="contain" 
+              source={require('../img/red-dress.png')}
+            />
+            <Image 
+              style={styles.image} 
+              resizeMode="contain" 
+              source={require('../img/blue-dress.png')}
+            />
+            <Image 
+              style={styles.image} 
+              resizeMode="contain" 
+              source={require('../img/green-dress.png')}
+            />
+            <Image 
+              style={styles.image} 
+              resizeMode="contain" 
+              source={require('../img/yellow-dress.png')}
+            />
+          </ScrollView>
+        </View>
         <Card />
       </View>
     );
@@ -25,7 +56,11 @@ const styles = StyleSheet.create({
   },
   images: {
     flex: 1,
-  }
+    marginTop: Platform.OS === 'ios' ? 20 : 0,
+  },
+  image: {
+    width: Dimensions.get('window').width,
+  },
 });
 
 export default clothCard;
