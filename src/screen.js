7b95cf00575c1869528f5181adfabc4b82b23c01
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-02-18 12:06:13 +0200

    Put all screen thing to one file

---
+++ src/screen.js
@@ -0,0 +1,6 @@
+import {Dimensions} from 'react-native';
+
+export const {width: screenWidth} = Dimensions.get('window');
+export const {height: screenHeight} = Dimensions.get('window');
+export const halfScreenWidth = screenWidth / 2;
+export const halfScreenHeight = screenHeight / 2;
