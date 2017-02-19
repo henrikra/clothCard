902826c53b8992bda72fd8dcb41f2d343fe701d4
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-02-19 21:05:07 +0200

    Fix screen height by platform

--- src/screen.js
+++ src/screen.js
@@ -1,6 +1,8 @@
 import {Dimensions} from 'react-native';
 
+import {isAndroid} from './platform';
+
 export const {width: screenWidth} = Dimensions.get('window');
-export const {height: screenHeight} = Dimensions.get('window');
+export const screenHeight = Dimensions.get('window').height + (isAndroid ? -20 : 20);
 export const halfScreenWidth = screenWidth / 2;
 export const halfScreenHeight = screenHeight / 2;
