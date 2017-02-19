cf0a8640b4abae79188c12123bc17de2084d92e3
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-02-19 21:25:47 +0200

    Enable layoutanimation on Android

--- index.android.js
+++ index.android.js
@@ -1,5 +1,7 @@
-import {AppRegistry} from 'react-native';
+import {AppRegistry, UIManager} from 'react-native';
 
 import App from './src/components/App';
+
+UIManager.setLayoutAnimationEnabledExperimental(true);
 
 AppRegistry.registerComponent('clothCard', () => App);
